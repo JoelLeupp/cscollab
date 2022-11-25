36272f02d56988440bd5aa5effcdc719de3218eb
Joel Leupp <joelleupp@gmail.com>
2022-11-25 15:45:47 +0100

    get geo-coordinates with geoapify

--- data_generation/sandbox.py
+++ data_generation/sandbox.py
@@ -8,53 +8,54 @@ print(csrankings.describe())
 country_info = pd.read_csv("country-info.csv")
 print(country_info.describe())
 
-# get missing affiliation in the country-info.csv
-missing_inst= list(filter(lambda x: x not in country_info["institution"].to_list(), 
-       csrankings["affiliation"].unique()))
+# view inst-geo-map.csv
+inst_geo_map = pd.read_csv("inst-geo-map.csv")
+print(inst_geo_map.describe())
 
-# map canada to the northamerica region
-country_info["region"].replace("canada", "northamerica", inplace=True)
+# get geo-coordinates of institutions
+# use geoapify
+import requests
+from requests.structures import CaseInsensitiveDict
+from fuzzywuzzy import fuzz
 
-# strip whitespaces in region
-country_info["region"] = list(map(lambda x: x.strip(), country_info["region"]))
+def geo_api_url(institution, country):
+    text = "{}, {}".format(institution,country)
+    api_key = "e929c302233e42a285148887db8e42e2"
+    url = "https://api.geoapify.com/v1/geocode/search?text={}&apiKey={}".format(text, api_key)
+    return url
 
-# all missing affiliations are in the USA
-# add us affiliation to country-info
-country_info = pd.concat([country_info,
-                          pd.DataFrame(list(map(lambda x: [x,"northamerica" , "us"], missing_inst)),
-                                 columns=country_info.columns)]) 
-
-# add country names to country-info
-country_names_csv = "https://pkgstore.datahub.io/core/country-list/data_csv/data/d7c9d7cfb42cb69f4422dec222dbbaa8/data_csv.csv"
-country_names = pd.read_csv(country_names_csv)
-
-def get_country_name(abbrv):
-    code = abbrv.upper()
-    if code in country_names["Code"].to_list():
-        return country_names["Name"][country_names["Code"] == code].values[0]
-    elif abbrv == "uk":
-        return "United Kingdom" 
-    else:
-        return "unknown"
-
-country_info["countryname"] = list(map(get_country_name, country_info["countryabbrv"]))
-
-# add region names to country-info
-regions = {"europe": "Europe", 
-           "africa": "Africa", 
-           "asia" :"Asia",
-           "australasia": "Australasia",
-           "northamerica": "North America",
-           "southamerica" : "South America"} 
-
-country_info["regionname"] = list(map(lambda x: regions[x], country_info["region"]))
-
-# rearange and rename columns
-cols = ['institution', 'countryabbrv', 'countryname', 'region','regionname']
-country_info = country_info[cols]
-country_info.columns = ['institution', 'country_code', 'country_name', 'region_code','region_name']
-country_info
+headers = CaseInsensitiveDict()
+headers["Accept"] = "application/json"
 
 
+geo_codes = []
+# institution = "Bielefeld University"
+# country = "Germany"
+for _, row in inst_geo_map.iterrows():
+    institution = row["institution"]
+    country = row["country_name"]
+    resp = requests.get(geo_api_url(institution, country), headers=headers)
+    res = resp.json()
+
+    # sort results by importance and only take the most important result
+    features = res["features"]
+    if features == []:
+        print(institution)
+        break
+    # if len(features) > 1:
+    #     features = sorted(features, key = lambda x: x["properties"]["rank"]["importance"], reverse=True)
+    feature = features[0]
+    properties = feature["properties"]
+    if properties["result_type"] == "amenity":
+        prop = {"institution" : institution,
+                "name" : properties["name"],
+                "lat" : properties["lat"] ,
+                "lon": properties["lon"],
+                "fuzz-ratio": fuzz.ratio(institution, properties["name"])}
+        geo_codes.append(prop)
+    else: 
+        print(institution)
+        break
+    
 
 
