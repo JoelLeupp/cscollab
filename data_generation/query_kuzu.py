c93e12dd75e78cee4c0f1d26a65db2860150b351
Joel <joelleupp@gmail.com>
2023-01-10 08:53:17 +0100

    basic queries

--- data_generation/query_kuzu.py
+++ data_generation/query_kuzu.py
@@ -11,6 +11,31 @@ db = kuzu.database('./kuzu_db')
 conn = kuzu.connection(db)
 
 # get country region mapping
-country_mapping = conn.execute('''  MATCH (c:Country)-[i:InRegion]->(r:Region) 
-                                    RETURN *;''').getAsDF()            
-print(country_mapping.head(),"\n", country_mapping.shape)
+country_map = conn.execute('''  
+                MATCH (c:Country)-[i:InRegion]->(r:Region) 
+                RETURN *;
+                ''').getAsDF()            
+print(country_map.head(),"\n", country_map.shape)
+
+# get computer science area and sub-areas
+area_mapping = conn.execute('''  
+                MATCH (s:SubArea)-[o:SubAreaOf]->(a:Area) 
+                RETURN *;
+                ''').getAsDF()            
+print(area_mapping,"\n", area_mapping.shape)
+
+# get all proceedings that are categorized with an area
+proceedings_all = conn.execute('''  
+                MATCH (p:Proceeding)-[b:BelongsToArea]->(a:SubArea) 
+                RETURN  p;
+                ''').getAsDF()            
+print(proceedings_all.head(),"\n", proceedings_all.shape)
+
+# get all proceedings of a subarea
+sub_area = "vision"
+proceedings_in_sub = conn.execute('''  
+                MATCH (p:Proceeding)-[b:BelongsToArea]->(a:SubArea) 
+                WHERE a.id = "{}"
+                RETURN  p;
+                '''.format(sub_area)).getAsDF()            
+print(proceedings_in_sub.head(),"\n", proceedings_in_sub.shape)
