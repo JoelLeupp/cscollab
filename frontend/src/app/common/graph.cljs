cbe9c41711fe781f4a2d2f3d29b42836485cdd89
Joel Leupp <joelleupp@gmail.com>
2023-02-20 16:40:32 +0100

    graph vis

--- frontend/src/app/common/graph.cljs
+++ frontend/src/app/common/graph.cljs
@@ -4,9 +4,38 @@
    ["cytoscape-dagre" :as dagre]
    ["cytoscape-fcose" :as fcose]
    [app.util :as util]
-   [re-frame.core :refer (dispatch subscribe)]
+   [re-frame.core :as rf :refer
+    (dispatch reg-event-fx reg-fx reg-event-db reg-sub subscribe)]
    [app.db :as db]
    ["react-cytoscapejs" :as rcy]))
+
+
+(reg-sub
+ ::graph
+ (fn [db _] (:graph db)))
+
+(reg-sub
+ ::graph-field
+ :<- [::graph]
+ (fn [m  [_ id]]
+   (let [id (if (vector? id) id [id])]
+     (get-in m id))))
+
+(reg-event-fx
+ ::set-graph-field
+ (fn [{db :db} [_ id value]]
+   (let [id (if (vector? id) id [id])]
+     {:db (assoc-in db (into [:graph] id) value)})))
+
+(reg-sub
+ ::info-open?
+ :<- [::graph-field :info-open?]
+ (fn [m] (when m m)))
+
+(reg-sub
+ ::elements
+ :<- [::graph-field :elements]
+ (fn [g] (when g g)))
 
 (defn init-cytoscape-dagre []
   (.use cy dagre))
@@ -15,21 +44,23 @@
   (.use cy fcose))
 
 (defn graph [{:keys [elements layout style stylesheet on-click cy]}]
-  (fn [{:keys [id elements layout style stylesheet on-click]}]
-    (let [cy (or cy (atom nil))
-          loaded? (atom false)]
+  (let [cy (or cy (atom nil))
+        loaded? (atom false)]
+    (fn [{:keys [id elements layout style stylesheet on-click]}] 
       [:> rcy
        {:cy
         #(do
            (reset! cy %)
+           (dispatch [::set-graph-field [:cy] @cy])
            (when id
              (dispatch [::db/set-ui-states [:graph id] %]))
            (when-not @loaded?
              (reset! loaded? true)
              (.on % "tap" "node" (fn [event] (when on-click (on-click event))))
+             (.on % "tap" "edge" (fn [event] (when on-click (on-click event))))
              (.on % "tap" (fn [e]
                             (when (= (.-target e) %)
-                              (dispatch [::db/set-ui-states [:graph :detail?] false]))))))
+                              (dispatch [::set-graph-field [:info-open?] false]))))))
         :elements elements
         :layout layout
         :style (util/deep-merge
--- frontend/src/app/cscollab/graph_view.cljs
+++ frontend/src/app/cscollab/graph_view.cljs
@@ -16,8 +16,57 @@
              (dispatch reg-event-fx reg-fx reg-event-db reg-sub subscribe)]
             [app.util :as util]))
 
+(declare gen-edges)
+(declare gen-nodes)
 
-(def elements
+(defn gen-elements []
+  (let [insti? @(subscribe [::mp/insti?])
+        weighted-collab
+        (tf/weighted-collab {:insti? insti?})
+        csauthors
+        @(subscribe [::data/csauthors])
+        geo-mapping-inst
+        (zipmap (map :institution csauthors)
+                (map #(hash-map
+                       :id (:institution %)
+                       :name (:institution %)) csauthors)) 
+        geo-mapping-author
+        (zipmap (map :pid csauthors)
+                (map #(hash-map
+                       :id (:pid %)
+                       :institution (:institution %)
+                       :name (:name %)) csauthors))
+        geo-mapping
+        (merge geo-mapping-inst geo-mapping-author)]
+    (if (and weighted-collab csauthors geo-mapping) 
+      (vec
+       (concat
+        (gen-edges weighted-collab geo-mapping)
+        (gen-nodes weighted-collab geo-mapping)))
+      [])))
+
+
+(defn gen-nodes [weighted-collab geo-mapping]
+  (let [nodes (vec (clojure.set/union
+                    (set (map :node/m weighted-collab))
+                    (set (map :node/n weighted-collab))))]
+    (mapv #(hash-map :data
+                     {:id %
+                      :label (get-in geo-mapping [% :name])})
+          nodes)))
+
+(defn gen-edges [weighted-collab geo-mapping] 
+  (mapv
+   #(let [node-m (get geo-mapping (:node/m %))
+          node-n (get geo-mapping (:node/n %))]
+      (when (and node-m node-n)
+        (hash-map :data
+                  {:id (str (:id node-m) "_" (:id node-n))#_[(:id node-m) (:id node-n)]
+                   :source (:id node-m)
+                   :target (:id node-n)})))
+   weighted-collab))
+
+(def elements-test
   [{:data {:id :a :label "A"}}
    {:data {:id :b :label "B"}}
    {:data {:id :f :label "F"}}
@@ -31,37 +80,38 @@
    {:data {:source :g :target :h}}
    {:data {:source :h :target :f}}])
 
-(defn graph-comp []
-  (g/init-cytoscape-dagre)
-  (fn []
-    [g/graph {:on-click (fn [e] (js/console.log (.. e -target data)))
-              :elements elements
-              :layout {:name :grid}
-              :stylesheet  [{:selector "node"
-                             :style {:background-color (fn [ele]
-                                                         (get (->clj (.. ele data)) :bg (:main colors)))
-                                     :shape (fn [ele] (or (.. ele data -shape) "ellipse"))
-                                     :width "label"
-                                     :height "label"
-                                     :padding 6}}
-                            {:selector "node[type=\"exit\"]"
-                             :style {:background-color :black}}
-                            {:selector "node:selected"
-                             :style {:background-color (:second colors)}}
-                            {:selector "edge:selected"
-                             :style {:line-color (:second colors)}}
-                            {:selector "node[label]"
-                             :style {:label "data(label)"
-                                     :color :white
-                                     :font-size 6
-                                     :text-halign :center
-                                     :text-valign :center}}
-                            {:selector :edge
-                             :style {:curve-style :bezier
-                                     :target-arrow-shape :triangle
-                                     :arrow-scale 0.5
-                                     :width 1}}]
-              :style {:width "100%" :hight "100%" :background-color "white"}}]))
+(defn graph-comp [] 
+  (let [elements (subscribe [::g/elements])] 
+    (fn []
+      (when (empty? @elements)
+        (dispatch [::g/set-graph-field [:elements] (gen-elements)])) 
+      (when @elements
+        ^{:key @elements}
+        [g/graph {:on-click (fn [e] (js/console.log (.. e -target data)))
+                  :elements @elements
+                  :layout {:name :grid}
+                  :stylesheet  [{:selector "node"
+                                 :style {:background-color (fn [ele]
+                                                             (get (->clj (.. ele data)) :bg (:main colors)))
+                                         :shape (fn [ele] (or (.. ele data -shape) "ellipse"))
+                                         :width "label"
+                                         :height "label"
+                                         :padding 6}}
+                                {:selector "node[type=\"exit\"]"
+                                 :style {:background-color :black}}
+                                {:selector "node:selected"
+                                 :style {:background-color (:second colors)}}
+                                {:selector "edge:selected"
+                                 :style {:line-color (:second colors)}}
+                                {:selector "node[label]"
+                                 :style {:label "data(label)"
+                                         :color :white
+                                         :font-size 6
+                                         :text-halign :center
+                                         :text-valign :center}}
+                                {:selector :edge
+                                 :style {:width 1}}]
+                  :style {:width "100%" :hight "100%" :background-color "white"}}]))))
 
 (defn graph-view []
   (let [insti? (subscribe [::mp/insti?])]
@@ -75,4 +125,12 @@
                          [:h3 {:style {:margin 0}} "INFO BOX"]
                          [button/close-button
                           {:on-click  #(dispatch [::db/set-ui-states [:viz :open?] false])}]]
-        :info-open? (subscribe [::db/ui-states-field [:viz :open?]])}])))
+        :info-open? (subscribe [::db/ui-states-field [:viz :open?]])
+        :update-event #(dispatch [::g/set-graph-field [:elements] (gen-elements)])}])))
+
+(comment
+  (def elements @(subscribe [::g/elements]))
+  elements
+  (subvec elements 0 5)
+  (last elements)
+  )
