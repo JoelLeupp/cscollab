0fcb355ccf3970faed9da988b4fa807de7d87c75
Joel Leupp <joelleupp@gmail.com>
2023-04-24 13:21:03 +0200

    config

--- backend/settings.py
+++ backend/settings.py
@@ -5,8 +5,9 @@ from apispec.ext.marshmallow import MarshmallowPlugin
 class Config(object):
     """Base configuration."""
     APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
-    CORS_ORIGIN_WHITELIST = ["http://localhost:8090", "https://cscollab.ifi.uzh.ch"]
+    CORS_ORIGIN_WHITELIST = ["http://localhost:8090", "https://cscollab.ifi.uzh.ch","https://cscollab.ifi.uzh.ch/backend"]
     #STATIC_FOLDER = "./public"
+    SERVER_NAME = "localhost:8030"
     APISPEC_SWAGGER_UI_URL = None
     APISPEC_SPEC = APISpec(
         title='CSCOLLAB BACKEND APIS',
